659acfd2d8745bb231ab9f19fb4aaa06b75da004
alan9487 <79899890+alan9487@users.noreply.github.com>
2023-06-04 11:15:41 +0800

    Create ShapeType.h

---
+++ ShapeType.h
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <conio.h>
+#ifndef s_h
+#define s_h
+
+using namespace std;
+
+enum ShapeType {
+    SHAPE_EMPTY,
+    SHAPE_I,
+    SHAPE_J,
+    SHAPE_L,
+    SHAPE_O,
+    SHAPE_S,
+    SHAPE_T,
+    SHAPE_Z
+};
+
+#endif
